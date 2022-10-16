3142a6bb9763ea48e67693bced62844b0736d982
Alexander Juda <alexanderjuda@gmail.com>
2022-10-16 20:51:48 +0200

    Add tests for iso

--- tests/sickdt/test_core.py
+++ tests/sickdt/test_core.py
@@ -1,5 +1,8 @@
-import sickdt
 from datetime import datetime, timezone, timedelta
+
+import pytest
+
+import sickdt
 
 
 class TestNow:
@@ -57,6 +60,55 @@ class TestUnix:
                 )
                 ts = sickdt.unix(instant1)
                 instant2 = sickdt.from_unix(ts)
+
+                assert instant2.astimezone(timezone.utc) == instant1.astimezone(
+                    timezone.utc
+                )
+
+
+class TestIso:
+    class TestRoundtrip:
+        @staticmethod
+        def test_iso_instant_iso():
+            iso1 = "2022-10-16T18:49:40.002137Z"
+            instant = sickdt.from_iso(iso1)
+            iso2 = sickdt.iso(instant)
+
+            assert iso2 == iso1
+
+        class TestInstantTsInstant:
+            @staticmethod
+            def test_initial_utc():
+                instant1 = datetime(
+                    year=2022,
+                    month=10,
+                    day=15,
+                    hour=21,
+                    minute=37,
+                    second=25,
+                    microsecond=42,
+                    tzinfo=timezone.utc,
+                )
+                iso = sickdt.iso(instant1)
+                instant2 = sickdt.from_iso(iso)
+
+                assert instant2 == instant1
+
+            @staticmethod
+            def test_non_utc():
+                start_tz = timezone(timedelta(hours=2))
+                instant1 = datetime(
+                    year=2022,
+                    month=10,
+                    day=15,
+                    hour=21,
+                    minute=37,
+                    second=25,
+                    microsecond=42,
+                    tzinfo=start_tz,
+                )
+                iso = sickdt.iso(instant1)
+                instant2 = sickdt.from_iso(iso)
 
                 assert instant2.astimezone(timezone.utc) == instant1.astimezone(
                     timezone.utc
